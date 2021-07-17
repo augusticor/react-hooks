8293bd4b748d17d74073f72f50e40f2fbfb4695c
augusticor <rojascruzoscar@gmail.com>
2021-07-17 18:10:29 -0500

    Form custom hook
    - useForm custom hook
    - FormWithCustomHook component

---
+++ src/components/02-useEffect/FormWithCustomHook.js
@@ -0,0 +1,69 @@
+import React, { useEffect } from 'react';
+
+import { useForm } from '../hooks/useForm';
+
+import './effects.css';
+
+const FormWithCustomHook = () => {
+	// Usando ahora el useForm custom hook
+	const [formValues, handleInputChange] = useForm({
+		name: '',
+		email: '',
+		password: '',
+	});
+
+	const { name, email, password } = formValues;
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(formValues);
+	};
+
+	return (
+		<>
+			<h1>Form with custom hook</h1>
+			<hr />
+			<form onSubmit={handleSubmit}>
+				<div className='form-group'>
+					<label htmlFor='nameInput'>User Name</label>
+					<input
+						id='nameInput'
+						type='text'
+						className='form-control'
+						placeholder='Your name'
+						name='name'
+						autoComplete='off'
+						value={name}
+						onChange={handleInputChange}
+					/>
+				</div>
+
+				<div className='form-group'>
+					<label htmlFor='emailInput'>Your email</label>
+					<input
+						id='emailInput'
+						type='email'
+						className='form-control'
+						placeholder='example@example.example'
+						name='email'
+						value={email}
+						onChange={handleInputChange}
+						required={true}
+					/>
+					<small className='form-text text-muted'>We'll never share your email with anyone else.</small>
+				</div>
+
+				<div className='form-group'>
+					<label htmlFor='passwordInput'>Password</label>
+					<input id='passwordInput' type='password' className='form-control' name='password' value={password} onChange={handleInputChange} required={true} />
+				</div>
+
+				<button type='submit' className='btn btn-success'>
+					Submit form
+				</button>
+			</form>
+		</>
+	);
+};
+
+export default FormWithCustomHook;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import CounterApp from './components/01-useState/CounterApp';
 import HooksApp from './HooksApp';
 import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 import SimpleForm from './components/02-useEffect/SimpleForm';
+import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
 
 const root = document.getElementById('root');
 
-ReactDOM.render(<SimpleForm />, root);
+ReactDOM.render(<FormWithCustomHook />, root);
