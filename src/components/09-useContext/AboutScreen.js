a64638e7d71a694c55f1d33d28c53542f862b574
augusticor <rojascruzoscar@gmail.com>
2021-08-02 18:51:37 -0500

    useContext React Hook
    - using useContext hook to pass a user object

--- src/components/09-useContext/AboutScreen.js
+++ src/components/09-useContext/AboutScreen.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { UserContext } from './UserContext';
 
 const AboutScreen = () => {
+	const { user, setUser } = useContext(UserContext);
+
+	const handleClick = () => {
+		setUser({});
+	};
+
 	return (
 		<div>
 			<h1>About</h1>
 			<hr />
+
+			<pre>{JSON.stringify(user, null, 4)}</pre>
+			<button className='btn btn-warning' onClick={handleClick}>
+				Bye bye !
+			</button>
 		</div>
 	);
 };
--- src/components/09-useContext/LoginScreen.js
+++ src/components/09-useContext/LoginScreen.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useForm } from '../hooks/useForm';
+
+import { UserContext } from './UserContext';
 
 const LoginScreen = () => {
+	const [{ userName, food }, handleInputChange, resetForm] = useForm({ userName: '', food: '' });
+
+	const { setUser } = useContext(UserContext);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setUser({ userName, food });
+		resetForm();
+	};
+
 	return (
 		<div>
 			<h1>Login Screen</h1>
 			<hr />
+
+			<form onSubmit={handleSubmit}>
+				<div className='form-group'>
+					<label htmlFor='textinput'>Name</label>
+					<input name='userName' type='text' value={userName} placeholder='Pepito Alimania' onChange={handleInputChange} />
+				</div>
+				<div className='form-group'>
+					<label htmlFor='textinput'>Favorite food</label>
+					<input name='food' type='text' value={food} placeholder='Pizza' onChange={handleInputChange} />
+				</div>
+				<button type='submit' className='btn btn-primary'>
+					Login
+				</button>
+			</form>
 		</div>
 	);
 };
